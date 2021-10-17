bffa45ddfc2535a2a5d8c5cb65d727a3792d434e
Kristof <you@example.com>
2021-10-17 19:01:35 +0200

    Elemek inicializálása

---
+++ app/src/main/java/com/example/coinflip/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.coinflip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgView1;
+    Button btn1, btn2;
+    TextView textView1, textView2, textView3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        imgView1 = findViewById(R.id.imgView1);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+    }
+}
